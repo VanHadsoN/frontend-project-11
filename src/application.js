6f23280dc4b2620dda94b0e3f9c5105a765d3f0a
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-08-11 14:51:35 +0300

    Fixing mentor-check. Changed "src\application.js"

--- src/application.js
+++ src/application.js
@@ -103,14 +103,10 @@ export default () => {
     const watchedState = onChange(initialState, render(elements, initialState, i18nInstance));
     getNewPosts(watchedState);
 
-    elements.form.addEventListener('input', (e) => {
+    elements.form.addEventListener('submit', (e) => {
       e.preventDefault();
       watchedState.process.processState = 'filling';
       watchedState.inputValue = e.target.value;
-    });
-
-    elements.form.addEventListener('submit', (e) => {
-      e.preventDefault();
       const urlList = watchedState.content.feeds.map(({ link }) => link);
 
       validate(watchedState.inputValue, urlList)
