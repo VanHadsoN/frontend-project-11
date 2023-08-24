7bbe1af37a88f222580284bd2a0504fe385fad65
VanHadsoN <107035951+VanHadsoN@users.noreply.github.com>
2023-08-24 11:22:06 +0300

    Fixing mentor-check. Changed "src\parser.js"

--- src/parser.js
+++ src/parser.js
@@ -6,7 +6,7 @@ export default (data) => {
     const error = new Error(parserError.textContent);
     error.isParsingError = true;
     error.data = data;
-    throw error;
+    throw new Error('parseError');
   }
   const channel = parsedData.querySelector('channel');
   const feedTitle = channel.querySelector('title').textContent;
